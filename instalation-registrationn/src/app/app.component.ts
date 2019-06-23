3db97ffcb09a0dc24be73fab9ab053eb4c2fe313
Avindu Hewa <avinduhewa@gmail.com>
2019-06-23 09:46:43 +0530

    finish data binding and macro integration

--- instalation-registrationn/src/app/app.component.ts
+++ instalation-registrationn/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +8,37 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
+  gscriptUrl = '/macros/s/AKfycbwZSM9uPJCyKKhVLPig9OL3FJEJkfGCup2Mgxy5tHOZyjgJo3o/exec';
   registered = false;
   loader = false;
   title = 'instalation-registrationn';
+  object = {
+    name: '',
+    email: '',
+    club: '',
+    designation: ''
+  };
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
   ngOnInit() {
 
   }
   register() {
-    this.registered = !this.registered;
+    this.loader = true;
+    console.log(this.object);
+    this.http.post(this.gscriptUrl, this.object).toPromise()
+      .then((res) => {
+        this.loader = false;
+        console.log('Response completed', res);
+        this.registered = !this.registered;
+      })
+      .catch((err) => {
+        this.loader = false;
+        console.log('Response failed', err);
+        this.registered = !this.registered;
+      });
   }
 }
